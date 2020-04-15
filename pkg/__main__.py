6697c5b92f287ae11dedab470ef99584ecf44076
stm107 <stm107@pitt.edu>
2020-04-15 19:07:27 -0400

    search returns appropiate(sp?) data

--- pkg/__main__.py
+++ pkg/__main__.py
@@ -1,11 +1,12 @@
 import occam_es
+import json
 
 cur_es = occam_es.Occam_es()
 cur_es.bulk_add("occam_objects.json")
 
 found = cur_es.occam_search(input("Word? "))
-# i = 1
-for line in found:
-    print('https://occam.software' + line['href'] + ' -> ' + line['summary'])
-# print(found)
+
+
+print(found)
+
 cur_es.clear_index('occam_index')
--- pkg/occam_es.py
+++ pkg/occam_es.py
@@ -3,7 +3,18 @@ from elasticsearch import helpers
 import elasticsearch_dsl
 import json
 
-
+    
+def help_return(request_obj):
+    obj_dict = request_obj.to_dict()
+    target = obj_dict.get('hits')['hits']
+    tmp=[]
+    for line in target:  
+        search_info = line.get('_source').get('return_obj')
+        tmp.append(search_info)
+        # for line in b:
+        #     tmp.append(line.get('_source').get('return_obj'))   
+    # print(tmp) 
+    return tmp
 class Occam_es:
     def __init__(self, host='http://localhost', port=9200):
         self.es = Elasticsearch(HOST=host, PORT=port)
@@ -11,6 +22,7 @@ class Occam_es:
         
     #  very basic attampt at providing elastic search functionality for Occam
 
+    
     def bulk_add(self, occam_data):
         # adds a bunch'o items to the search index
         # this is for starting a new occam server and adding an existing database of objects
@@ -31,6 +43,20 @@ class Occam_es:
                     'architecture': data[line].get('architecture'), 
                     'environment': data[line].get('enviorment'),
                     'summary': data[line].get('summary'),
+                    'return_obj': {
+                        'uid': data[line].get('uid'),
+                        'id': data[line].get('id'),
+                        'revision': data[line].get('revision'),
+                        'name': data[line].get('name'),
+                        'owner': data[line].get('owner'),
+                        'summary': data[line].get('summary'),
+                        'type': data[line].get('type'),
+                        'subtype': data[line].get('subtype'),
+                        'architecture': data[line].get('architecture'),
+                        'environment': data[line].get('enviorment'),
+                        'images': data[line].get('images')
+                    }
+                    # 'full_obj': data[line],
                 }
                 actions.append(entry)
                 # es.index(index="games", doc_type="game_info", id=index_val, body=data[line])
@@ -46,12 +72,12 @@ class Occam_es:
         # not done just matches orgional test functionality (probably)
         # res = self.es.search(index=index_name, body={"from":0,"size":10,"query":{"match":{"description":search_input}}})
         res = elasticsearch_dsl.Search(using=self.es, index=index_name)
-        print('<for debug/dev> number of objects in index ' + str(res.count()))
+        # print('<for debug/dev> number of objects in index ' + str(res.count()))
         q = elasticsearch_dsl.Q('multi_match', query=search_input, fields=['name', 'summary', 'environment'])
         ans = res.query(q)
         answer = ans.execute()
-        # print(answer)
-        return answer
+        # returns an array of dicts that conatin the data occam requres from search
+        return help_return(answer)
 
     def clear_index(self, index_name='occam_index'):
         self.es.indices.delete(index=index_name)
